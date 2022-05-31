f88efb1572be1e3b12784b37f37ca6986f22da0d
Shiva <shivakumarsunderamurthy@gmail.com>
2022-06-01 00:17:12 +0800

    added homepage css design and contents

--- ecom_vue/src/App.vue
+++ ecom_vue/src/App.vue
@@ -1,12 +1,84 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="wrapper">
+    <nav class="navbar">
+      <div class="nav-start">
+        <router-link to="/" class="test" id="test-me"
+          ><strong>Caps</strong></router-link
+        >
+        <form class="test" method="get" action="/search">
+          <div class="field has-addons">
+            <div class="control">
+              <input
+                type="text"
+                class="input"
+                placeholder="What are you looking for?"
+                name="query"
+              />
+            </div>
+
+            <div class="control">
+              <button class="button is-success">
+                <span class="icon">
+                  <i class="fas fa-search"></i>
+                </span>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <ul class="nav-end">
+        <li><router-link to="/">Sports</router-link></li>
+        <li><router-link to="/">Fashion</router-link></li>
+        <li><router-link to="/">My account</router-link></li>
+        <li><router-link to="/">Cart</router-link></li>
+      </ul>
+    </nav>
+  </div>
+
+  <router-view />
 </template>
 
 <style lang="scss">
-@import '../node_modules/bulma';
+@import "../node_modules/bulma";
 
+* {
+  font-family: "Poppins", sans-serif;
+}
+
+.wrapper {
+  width: 100%;
+  height: 64px;
+  background-color: #1d2518;
+}
+.navbar {
+  height: 64px;
+  max-width: 80%;
+  background-color: #1d2518;
+  color: #ffffff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.nav-start .test {
+  display: inline-block;
+  // padding: 0px 20px;
+  padding-right: 20px;
+  vertical-align: middle;
+}
+
+.nav-end {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+a {
+  color: #ffffff;
+}
+.nav-end li {
+  display: inline-block; //side by side list
+  padding-right: 20px;
+}
 </style>
--- ecom_vue/src/views/HomeView.vue
+++ ecom_vue/src/views/HomeView.vue
@@ -1,18 +1,124 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container-1">
+      <img
+        class="home-pic"
+        :src="require(`@/assets/hossein-azarbad-AP_qief3n94-unsplash.jpg`)"
+      />
+      <div class="welcome">
+        <p class="title mb-6 has-text-white">Welcome to caps</p>
+        <p class="subtitle has-text-white">The best caps store online</p>
+      </div>
+    </div>
+    <div class="container-2">
+      <div class="is-size-2 has-text-centered">Latest Products</div>
+      <div class="sub-container">
+        <div class="box-1">
+          <div class="image-container">
+            <img
+              :src="require(`@/assets/erik-mclean-WIejoQqIvhE-unsplash.jpg`)"
+              alt=""
+            />
+          </div>
+          <p>box 1</p>
+        </div>
+        <div class="box-1">
+          <div class="image-container">
+            <img
+              :src="
+                require(`@/assets/claudio-schwarz-PH8GUKG-Do0-unsplash.jpg`)
+              "
+              alt=""
+            />
+          </div>
+          <p>box 2</p>
+        </div>
+        <div class="box-1">
+          <div class="image-container">
+            <img
+              :src="require(`@/assets/dave-goudreau-FEx5z_k0hss-unsplash.jpg`)"
+              alt=""
+            />
+          </div>
+          <p>box 3</p>
+        </div>
+        <div class="box-1" id="sad">
+          <div class="image-container">
+            <img
+              :src="require(`@/assets/erik-mclean-WIejoQqIvhE-unsplash.jpg`)"
+              alt=""
+            />
+          </div>
+          <p>box 4</p>
+        </div>
+      </div>
+    </div>
   </div>
+  <br />
+  <br />
+  <br />
+  <br />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
+
+<style>
+.home-pic {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 80%;
+  height: 500px;
+  padding-top: 30px;
+}
+
+.container-1 {
+  position: relative;
+  text-align: center;
+  color: white;
+  margin-bottom: 45px;
+}
+
+.welcome {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.container-2 {
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.sub-container {
+  display: flex;
+  justify-content: space-between;
+}
+.box-1 {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); /* this adds the "card" effect */
+  background-color: #f1f1f1;
+  width: 20%;
+  padding: 16px;
+  margin: auto;
+}
+.box-1 img {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  margin: auto;
+}
+.image-container {
+  height: 300px;
+  width: 200px;
+  margin: auto;
+}
+</style>
